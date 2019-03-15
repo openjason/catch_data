bbd2ab3f40a529d88a475c754c0d3f69f0428541
openjason <openjc@outlook.com>
2019-03-15 17:30:59 +0800

    Update fetch_ip.py

--- fetch_local_ext_ip/fetch_ip.py
+++ fetch_local_ext_ip/fetch_ip.py
@@ -53,6 +53,29 @@ console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 #################################################################################################
 
+#coding:utf-8
+'''
+f_name为所读xx.txt文件
+输出为：文件最后一行
+'''
+def get_old_ip_from_txt():
+    fname = 'lastip.log'
+    with open(fname, 'r') as f:  #打开文件
+        first_line = f.readline()  #读第一行
+        off = -50      #设置偏移量
+        while True:
+            f.seek(off, 2) #seek(off, 2)表示文件指针：从文件末尾(2)开始向前50个字符(-50)
+            lines = f.readlines() #读取文件指针范围内所有行
+            if len(lines)>=2: #判断是否最后至少有两行，这样保证了最后一行是完整的
+                last_line = lines[-1] #取最后一行
+                break
+            #如果off为50时得到的readlines只有一行内容，那么不能保证最后一行是完整的
+            #所以off翻倍重新运行，直到readlines不止一行
+            off *= 2
+        print('文件' + fname + '第一行为：' + first_line)
+        print('文件' + fname + '最后一行为：'+ last_line)
+    return (last_line)
+
 def send_email(dir_path,files,toaddr,ccaddr,c_name,c_subject):
     logging.info("Subject:"+c_subject)
     msg = MIMEMultipart()
@@ -120,6 +143,7 @@ def fetch_localhost_inet_ip():
         return ("fetch ip address FAILD.")
 
 if __name__ == "__main__":
+    last_ip_from_txt = get_old_ip_from_txt()
     inet_ip = fetch_localhost_inet_ip()
     logging.info("fetch_localhost_inet_ip: "+inet_ip)
     print(inet_ip)
@@ -131,6 +155,7 @@ if __name__ == "__main__":
     ccmail_list = ''
     c_subject = inet_ip
     print (folder_list,file_list,SMTP_to_email_addr,ccaddr,c_name,c_subject)
+
     send_email(folder_list,file_list,SMTP_to_email_addr,ccaddr,SMTP_USER,c_subject)
     logging.info("正在发送邮件..."+c_name)
     time.sleep(1)
