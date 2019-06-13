e774fd739b6f40252999aa796531fd32744a2143
openjason <openjc@outlook.com>
2019-06-13 08:36:36 +0800

    0613

--- winauto/folderclean.py
+++ winauto/folderclean.py
@@ -1,6 +1,6 @@
 # 对指定文件夹内文件进行清理，文件创建日期超指定时间的文件将删除，并记录日志
 # author jc
-# 编写时间：2019-05-14
+# 编写时间：2019-06-12
 # 用途：
 # 清理指定文件夹特定文件
 # 功能：
@@ -18,32 +18,42 @@
 # 备注：需在系统计划任务添加相应任务，自动定时调用本脚本程序。
 
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 import sys
 import configparser
 import time
 import string
+import platform
 
 
 def GetDesktopPath():
-    return os.path.join(os.path.expanduser("~"), 'Desktop')
+    platform_string = platform.platform()
+    logging.info(platform_string)
+    if 'Windows-7' in platform_string:
+        return os.path.join(os.path.expanduser("~"), 'Desktop')
+    if 'Windows-XP' in platform_string:
+        return os.path.join(os.path.expanduser("~"), '桌面')
 
 def set_logging(logfile_path):
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(levelname)s %(message)s',
-                        datefmt='%a, %d %b %Y %H:%M:%S',
-                        filename = logfile_path + '\\folderclean.log',
-                        filemode='a')
-
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    logging.getLogger('').addHandler(console)
+    global logger
+    logger = logging.getLogger('my_logger')
+    handler = RotatingFileHandler(logfile_path + '\\folderclean.log', maxBytes=2000000, backupCount=6)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    formatter = logging.Formatter('%(asctime)-12s  %(message)s')
+    handler.setFormatter(formatter)
 
 def check_dir(work_dir, dtime, filepatten_list):
     source_dir = work_dir
     FileList = []
+
+    #若为空，则直接返回
+    if len(work_dir) < 1:
+        return FileList
+
     if not os.path.exists(source_dir):
-        logging.info('无法打开文件夹：' + source_dir)
+        logger.info('无法打开文件夹：' + source_dir)
     else:
         have_jdb_file = False
         for i in os.listdir(source_dir):
@@ -51,16 +61,17 @@ def check_dir(work_dir, dtime, filepatten_list):
             statinfo = os.stat(fullname)
             howlong  = statinfo.st_ctime - time.time()
             file_ctime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(statinfo.st_ctime))
-            print(fullname + ' 文件创建时间:' + file_ctime)
+            #print(fullname + ' 文件创建时间:' + file_ctime)
+            logger.info(fullname + ' 文件创建时间:' + file_ctime)
             if abs(howlong/60) > dtime :
                 for j in range(len(filepatten_list)):
                     if filepatten_list[j] in os.path.basename(fullname):
                         print(fullname + ' match patten:' + filepatten_list[j])
                         try:
                             os.remove(fullname)
-                            logging.info('删除文件' + fullname + ' 文件创建时间:' + file_ctime)
+                            logger.info('删除文件' + fullname + ' 文件创建时间:' + file_ctime)
                         except:
-                            logging.info(fullname + ' 文件删除失败')
+                            logger.info(fullname + ' 文件删除失败')
         return FileList
 
 if __name__ == '__main__':
@@ -82,7 +93,7 @@ if __name__ == '__main__':
         filepatten_list = filepatten.split('|')
         folder_list = folder_str.split('|')
     except Exception as e:
-        logging.info(e)
+        logger.info(e)
         exit(1)
     work_dir = GetDesktopPath()
     check_dir(work_dir, dtime, filepatten_list)
