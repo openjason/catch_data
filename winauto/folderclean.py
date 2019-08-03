49f767ca16e9e57994f8a699bc337992e601a245
openjason <openjc@outlook.com>
2019-08-03 20:55:53 +0800

    0803

--- winauto/folderclean.py
+++ winauto/folderclean.py
@@ -1,106 +1,195 @@
-# 对指定文件夹内文件进行清理，文件创建日期超指定时间的文件将删除，并记录日志
-# author jc
-# 编写时间：2019-06-12
-# 用途：
-# 清理指定文件夹特定文件
-# 功能：
-# 脚本用于清理指定文件夹内超过指定时间（文件创建时间）的文件。
-# 配置文件：
-# folderclean.conf
-# 参数说明：
-# 参数folder：待清理文件夹
-# 参数dtime：（单位：分钟）超过以上时间的文件将删除
-# 参数filepatten：文件名匹配
-# 脚本不适用：
-# 1、对只读文件、已打开或其他原因锁定文件无法执行删除。
-# 2、对文件夹内的文件夹无法执行删除。
-#
-# 备注：需在系统计划任务添加相应任务，自动定时调用本脚本程序。
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:洪卫
 
+import tkinter as tk  # 使用Tkinter前需要先导入
+import tkinter.messagebox
+import pickle
+from tkinter import scrolledtext # 导入滚动文本框的模块
+import time
 import logging
 from logging.handlers import RotatingFileHandler
-import os
+import os.path
 import sys
-import configparser
-import time
-import string
-import platform
-
 
 
 def set_logging(logfile_path):
     global logger
     logger = logging.getLogger('my_logger')
-    handler = RotatingFileHandler(logfile_path + '\\folderclean.log', maxBytes=2000000, backupCount=6)
+    handler = RotatingFileHandler(logfile_path + '\\barcode_check.log', maxBytes=5000000, backupCount=6)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
     formatter = logging.Formatter('%(asctime)-12s  %(message)s')
     handler.setFormatter(formatter)
 
-def GetDesktopPath():
-    platform_string = platform.platform()
-    logger.info(platform_string)
-    if 'Windows-7' in platform_string:
-        return os.path.join(os.path.expanduser("~"), 'Desktop')
-    elif 'Windows-XP' in platform_string:
-        return os.path.join(os.path.expanduser("~"), '桌面')
+
+# 第1步，实例化object，建立窗口window
+window = tk.Tk()
+
+# 第2步，给窗口的可视化起名字
+window.title('二维码内容检测工具')
+
+# 第3步，设定窗口的大小(长 * 宽)
+sw = window.winfo_screenwidth()
+sh = window.winfo_screenheight()
+ww = 900
+wh = 600
+x = (sw-ww)/2
+y = (sh-wh)/2
+window.geometry("%dx%d+%d+%d"%(ww,wh,x,y))  # 这里的乘是小x
+
+# 第4步，加载 wellcome image
+canvas = tk.Canvas(window, width=175, height=120, bg='green')
+image_file = tk.PhotoImage(file='ep.png')
+image = canvas.create_image(90, 0, anchor='n', image=image_file)
+canvas.pack(side='top')
+canvas.place(x=700,y=5)
+#tk.Label(window, text='Wellcome', font=('Arial', 16)).pack()
+# 第5步，用户信息
+tk.Label(window, text='标准二维码内容:', font=('Arial', 14)).place(x=20, y=25)
+
+sampling_entry_context = ''
+sampling_entry_context_len = 2
+
+int_compare_count =0
+int_compare_ok =0
+int_compare_diff =0
+
+
+# 第6步，用户登录输入框entry
+# 用户名
+svar_sampling_standard = tk.StringVar()
+svar_sampling_standard.set('请先点击初始化二维码内容...')
+entry_sampling_standard = tk.Entry(window, textvariable=svar_sampling_standard, width= 64, font=('Arial', 12))
+entry_sampling_standard.place(x=20, y=55)
+
+svar_sampling_curr = tk.StringVar()
+svar_sampling_curr.set('采样...')
+
+entry_sampling_curr = tk.Entry(window, textvariable=svar_sampling_curr, width= 64, font=('Arial', 12))
+entry_sampling_curr.place(x=20, y=140)
+
+
+scr = scrolledtext.ScrolledText(window)
+scr.place(x=20, y=170)
+
+svar_label_checkok = tk.StringVar()
+svar_label_checkok.set('二维码检测匹配正确 0次。')
+label_checkok = tk.Label(window, textvariable=svar_label_checkok, font=('Arial', 12))
+label_checkok.place(x=640, y=205)
+
+svar_label_checkfailed = tk.StringVar()
+svar_label_checkfailed.set('二维码检测匹配失败 0次。')
+label_checkfailed = tk.Label(window, textvariable=svar_label_checkfailed, font=('Arial', 12), fg='red')
+label_checkfailed.place(x=640, y=245)
+
+
+# 第8步，定义用户登录功能
+def barcode_check():
+    svar_sampling_curr.set('等待扫码')
+    entry_sampling_curr.focus_set()
+    entry_sampling_curr.selection_range(0,len(entry_sampling_curr.get()))
+    entry_sampling_curr.bind(sequence="<KeyRelease>", func=processKeyboardEvent)
+
+
+def processKeyboardEvent(ke):
+    global sampling_entry_context_len
+    str_sampling_curr =  entry_sampling_curr.get() #ke.char  # 按键对应的字符
+    #logger.info('s_len ' + str(sampling_entry_context_len))
+    if len(str_sampling_curr) > (sampling_entry_context_len - 1) :
+        #str_sampling_curr = str_sampling_curr + ke.char
+        logger.info("chars:" + str_sampling_curr)  # 按键对应的字符
+        string_compare(str_sampling_curr)
+        entry_sampling_curr.selection_range(0,len(entry_sampling_curr.get()))
+    #else:
+    #    logger.info("ch, len:" + str_sampling_curr + str(len(str_sampling_curr)))
+    
+def string_compare(str_curr):
+    global sampling_entry_context
+    global int_compare_ok
+    global int_compare_count
+    global int_compare_diff
+    global svar_label_checkok
+    global svar_label_checkfailed
+
+    str_action_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    print(sampling_entry_context,str_curr)
+    int_compare_count = int_compare_count +1
+    if sampling_entry_context == str_curr:
+        int_compare_ok = int_compare_ok +1
+        #scr.insert(tk.INSERT,str_action_time+': compare ok\n')
+        scr.insert(1.0, str_action_time + ': compare ok\n')
+        logger.info(': compare ok')
+        svar_label_checkok.set('二维码检测匹配正确 '+str(int_compare_ok)+'次。')
+
     else:
-        return os.path.join(os.path.expanduser("~"), 'Desktop')
+        int_compare_diff = int_compare_diff +1
+        #scr.insert(tk.INSERT, ': 注意：扫码内容与样本不匹配.\n')
+        scr.insert(1.0, str_action_time+': 注意：扫码内容与样本不匹配.\n')
+        logger.info(str_curr + ' :注意：扫码内容与样本不匹配.')
+        svar_label_checkfailed.set('二维码检测匹配失败 ' + str(int_compare_diff) + '次。')
+        
+
+def get_standard_content():
+    def button_caiyang():
+        entry_new_name.selection_range(0,len(entry_new_name.get()))
+        entry_new_name.focus_set()
+        # 这里是打开我们记录数据的文件，将注册信息读出
+
+    def button_return():
+        global sampling_entry_context_len
+        global sampling_entry_context
+
+        
+        sampling_entry_context = entry_new_name.get()
+        # 然后销毁窗口。
+        svar_sampling_standard.set(sampling_entry_context)
+        logger.info('svar_sampling_standard： ' + sampling_entry_context)
+        sampling_entry_context_len = len(sampling_entry_context)
+        svar_sampling_curr.set(sampling_entry_context_len)
+        svar_sampling_curr.set('请点击二维码内容检测')
+        print(sampling_entry_context_len)
+        window_sign_up.destroy()
+
+    # 定义长在窗口上的窗口
+    sw = window.winfo_screenwidth()
+    sh = window.winfo_screenheight()
+    ww = 540
+    wh = 300
+    x = (sw - ww) / 2
+    y = (sh - wh) / 2
+    window_sign_up = tk.Toplevel(window)
+    window_sign_up.geometry("%dx%d+%d+%d" % (ww, wh, x, y))
+    window_sign_up.title('标准卡二维码内容采样')
+
+    new_name = tk.StringVar()  # 将输入的注册名赋值给变量
+    new_name.set('等待扫码...')  # 将最初显示定为'example@python.com'
+    tk.Label(window_sign_up, text='二维码内容: ').place(x=10, y=10)  # 将`User name:`放置在坐标（10,10）。
+    entry_new_name = tk.Entry(window_sign_up, textvariable=new_name, width= 64,)  # 创建一个注册名的`entry`，变量为`new_name`
+    entry_new_name.place(x=10, y=30)  # `entry`放置在坐标（150,10）.
+
+    # 下面的 button_caiyang
+    #btn_comfirm_sign_up = tk.Button(window_sign_up, text='开始采样', command=button_caiyang)
+    #btn_comfirm_sign_up.place(x=380, y=150)
+    btn_comfirm_sign_up = tk.Button(window_sign_up, text='采样结束', command=button_return)
+    btn_comfirm_sign_up.place(x=440, y=210)
+    button_caiyang()
 
 
-def check_dir(work_dir, dtime, filepatten_list):
-    source_dir = work_dir
-    FileList = []
 
-    #若为空，则直接返回
-    if len(work_dir) < 1:
-        return FileList
 
-    if not os.path.exists(source_dir):
-        logger.info('无法打开文件夹：' + source_dir)
-    else:
-        have_jdb_file = False
-        for i in os.listdir(source_dir):
-            fullname = os.path.join(source_dir,i)
-            statinfo = os.stat(fullname)
-            howlong  = statinfo.st_ctime - time.time()
-            file_ctime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(statinfo.st_ctime))
-            #print(fullname + ' 文件创建时间:' + file_ctime)
-            logger.info(fullname + ' 文件创建时间:' + file_ctime)
-            if abs(howlong/60) > dtime :
-                for j in range(len(filepatten_list)):
-                    if filepatten_list[j] in os.path.basename(fullname):
-                        print(fullname + ' match patten:' + filepatten_list[j])
-                        try:
-                            os.remove(fullname)
-                            logger.info('删除文件' + fullname + ' 文件创建时间:' + file_ctime)
-                        except:
-                            logger.info(fullname + ' 文件删除失败')
-        return FileList
 
-if __name__ == '__main__':
-    os.path.abspath(sys.argv[0])
-    dirname, filename = os.path.split(os.path.abspath(sys.argv[0]))
-    realpathname, realfilename = os.path.split(os.path.realpath(sys.argv[0]))
+# 第7步，login and sign up 按钮
+btn_login = tk.Button(window, text='  二维码内容检测  ', command=barcode_check)
+btn_login.place(x=650, y=460)
+btn_sign_up = tk.Button(window, text='初始化二维码内容', command=get_standard_content)
+btn_sign_up.place(x=650, y=400)
 
-    set_logging(realpathname)
+os.path.abspath(sys.argv[0])
+dirname, filename = os.path.split(os.path.abspath(sys.argv[0]))
+realpathname, realfilename = os.path.split(os.path.realpath(sys.argv[0]))
 
-    cf = configparser.ConfigParser()
-    cffile = os.path.join(realpathname,"folderclean.conf")
-    try:
-        cf.read(cffile,encoding='GBK')
-        folder_str = cf.get("setting", "folder")
-        dtimestr = cf.get("setting", "dtime")
-        filepatten = cf.get("setting", "filepatten")
-        dtime = int(dtimestr)
-        filepatten = filepatten.replace('*','')
-        filepatten_list = filepatten.split('|')
-        folder_list = folder_str.split('|')
-    except Exception as e:
-        logger.info(e)
-        exit(1)
-    for im in range(len(folder_list)):
-        work_dir = folder_list[im]
-        check_dir(work_dir,dtime,filepatten_list)
-    work_dir = GetDesktopPath()
-    check_dir(work_dir, dtime, filepatten_list)
+set_logging(realpathname)
+
+# 第10步，主窗口循环显示
+window.mainloop()
